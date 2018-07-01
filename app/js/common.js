1a65de38b30d124abf78dedaa6d66ee64d89a6aa
Scordavis <scordavis@gmail.com>
2018-07-01 10:58:54 +0300

    init environment

---
+++ app/js/common.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	// Custom JS
+
+  // open reports
+$( '.app-content__disclose' ).click(function() {
+  $( this ).parent().find( '.app-content__wrapper' ).toggle( 'fast' );
+});
+
+});
